2c7dd2b6751f0e6d37ba190bc5ac74cc15a516c6
Sourashis Sarkar <116754962+Sourahsis@users.noreply.github.com>
2024-03-04 02:53:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,Response,jsonify
 import threading
 import cv2
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 import cv2
 import numpy as np
 import os
