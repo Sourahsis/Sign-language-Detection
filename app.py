472e4f2669e0fa034cfc4cfb8b92a231a3c8254e
Sourashis Sarkar <116754962+Sourahsis@users.noreply.github.com>
2024-03-03 21:15:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -160,6 +160,3 @@ def capture():
 @app.route('/get_prediction')
 def get_prediction():
     return jsonify({'predicted_action': predicted_actions})
-
-if __name__=="__main__":
-    app.run(debug=True)
